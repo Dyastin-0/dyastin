dfdeb059b7a58094c09c17a5ea86ebb4e97039f2
Justine <paralejasjustine15@gmail.com>
2025-01-09 02:09:18 +0800

    Focus on mount

--- src/components/terminal/TerminalInput.tsx
+++ src/components/terminal/TerminalInput.tsx
@@ -1,3 +1,5 @@
+import { useEffect, useRef } from "react";
+
 interface TerminalInputProps {
   setInput: React.Dispatch<React.SetStateAction<string>>;
   input: string;
@@ -8,18 +10,27 @@ const TerminalInput: React.FC<TerminalInputProps> = ({
   setInput,
   input,
   handelSubmit,
-}) => (
-  <form onSubmit={handelSubmit} className="flex mt-2">
-    <span className="text-primary-highlight">Dyastin@portfolio:~$</span>
-    <input
-      type="text"
-      className="flex-1 bg-transparent ml-2 outline-none text-primary-highlight placeholder-secondary-foreground"
-      value={input}
-      onChange={(e) => setInput(e.target.value)}
-      placeholder="Type a command..."
-      autoFocus
-    />
-  </form>
-);
+}) => {
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  useEffect(() => {
+    inputRef.current?.focus();
+  }, []);
+
+  return (
+    <form onSubmit={handelSubmit} className="flex mt-2">
+      <span className="text-primary-highlight">Dyastin@portfolio:~$</span>
+      <input
+        ref={inputRef}
+        type="text"
+        className="flex-1 bg-transparent ml-2 outline-none text-primary-highlight placeholder-secondary-foreground"
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+        placeholder="Type a command..."
+        autoFocus
+      />
+    </form>
+  );
+};
 
 export default TerminalInput;
